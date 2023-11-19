cd3367e7b35681140b1aadb65ebea3aee8312628
sangsokea <sangsokea109@gmail.com>
2023-11-19 13:57:48 +0700

    condition rendering

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,90 +1,33 @@
 import React, { useEffect, useState } from "react";
+import CardComponent from "../../components/common/card/CardComponent";
+import CardPlaceHolder from "../../components/common/card/CardPlaceHolder";
 
 export default function Home() {
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
   const [products, setProducts] = useState([]);
-
-  const [error, setError] = useState({
-    name: "",
-    message: "",
-  });
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    if (password.length < 6 && password.length > 0) {
-      setError({
-        name: "password",
-        message: "Password must be at least 6 characters",
-      });
-    } else {
-      setError({
-        name: "",
-        message: "",
-      });
-    }
-  }, [password]);
-
-  //   useEffect(callbackFunction, [dependencies])
-
-  useEffect(() => {
+    setLoading(true);
     fetch("https://dummyjson.com/products")
       .then((res) => res.json())
       .then((data) => {
+        // fetch successful
         setProducts(data.products);
+        setLoading(false);
       })
       .catch((err) => {
         console.log(err);
+        setLoading(false);
       });
   }, []);
 
-  const handleChange = (e) => {
-    setName(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
   return (
-    <div>
-      <h1 className="text-center text-4xl font-semibold text-blue-400">
-        List Product
-      </h1>
-      <h1 className="text-center">{name}</h1>
-      <form action="">
-        <div className="p-4 m-4">
-          <label htmlFor="name">Name: </label>
-
-          <input onChange={handleChange} type="text" id="name" />
-        </div>
-        <div className="p-4 m-4">
-          <label htmlFor="name">Password: </label>
-
-          <input
-            onChange={handlePasswordChange}
-            type="password"
-            id="password"
-          />
-        </div>
-        <div>
-          {error.name === "password" && error.message ? (
-            <p className="text-red-500">{error.message}</p>
-          ) : (
-            ""
-          )}
-        </div>
-      </form>
-
-      <div>
-        {products.map((product, index) => {
-          return (
-            <div key={index}>
-              <h1>{product.title}</h1>
-              <img src={product.images[0]} alt="" />
-            </div>
-          );
-        })}
-      </div>
+    <div className="flex flex-wrap justify-center gap-4 ">
+      {loading &&
+        [1, 2, 3, 4, 5, 6].map((item, index) => (
+          <CardPlaceHolder key={index} />
+        ))}
+      {!loading && products.map((item, index) => <CardComponent key={index} />)}
     </div>
   );
 }
